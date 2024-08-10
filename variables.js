78992f2acc7826e491c5fb2aaaadeb97412ef4a1
thealexsandro <jevringarmindo@gmail.com>
2024-08-10 08:05:46 +0700

    initial commit

--- variables.js
+++ variables.js
@@ -1,5 +1,5 @@
 const phone = [ 6285855633899, 62858556338991 ]
-const bwMsg = `I just made for backwards, I can't really answer any question.`
+const bwMsg = `I just made for backwards compatibility, I can't really answer any question.`
 
 const variables = {
     phone,
